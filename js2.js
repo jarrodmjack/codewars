4ab92c8a60dd0bfbee360a078e01d4039f7cb46e
jarrodmjack <jarrodmjack@gmail.com>
2022-04-26 23:14:58 -0400

    adding daily coding challenges

---
+++ js2.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+/* 
+Task
+Given a string str, reverse it omitting all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string
+
+*/
+
+
+function reverseLetter(str) {
+   let arr = str.split('').filter(item => isNaN(item))
+//    arr.forEach(item => {
+//        if(){
+//            console.log(item)
+//        }else{
+//            console.log('not string')
+//        }
+//    });
+let str2 = arr.reverse().join('')
+let filterSpecChar = str2.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+console.log(filterSpecChar)
+   }
+
+   reverseLetter("ultr53o@n")
