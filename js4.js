e793a7f05a28fba64b4f5fd24045fd51296536c4
jarrodmjack <jarrodmjack@gmail.com>
2022-04-29 23:32:43 -0400

    coding challenges

---
+++ js4.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+// function sumOfMinimums(arr) {
+//     let arr2 = arr.map(item => item.sort((a, b) => a - b))    
+//     let arr3 = arr2.map((item, i, self) => {
+//         return item[0]
+//     })
+//     console.log(arr3.reduce((acc, c) => acc + c))
+//   }
+
+
+//   sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]])
+
+
+
