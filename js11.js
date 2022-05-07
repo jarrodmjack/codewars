966a67f74de335f8036fb76d8d76a0a5429ab242
jarrodmjack <jarrodmjack@gmail.com>
2022-05-06 23:56:12 -0400

    coding challenge

--- js11.js
+++ js11.js
@@ -12,6 +12,75 @@ All values in the array will be integers. Return the good values in the order th
 
 
 
-function noOdds( values ){
-    return values.filter(item => item % 2 === 0)
+// function noOdds( values ){
+//     return values.filter(item => item % 2 === 0)
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+For example:
+
+([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+*/
+
+
+function evenNumbers(array, num) {
+    let newArr = array.filter(item => item % 2 === 0)
+    // console.log(newArr)
+
+    while(newArr.length > num){
+        newArr.shift()
+        console.log(newArr)
+    }
+
+
+    // console.log(newArr)
+
   }
+
+
+
+evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
