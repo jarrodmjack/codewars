da35a35263e6f5ae29960a045a7a9c4fa6f957af
jarrodmjack <jarrodmjack@gmail.com>
2022-05-08 23:46:56 -0400

    coding challenges

--- js13.js
+++ js13.js
@@ -193,3 +193,66 @@ checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
 // alphabetWar('zzzzs')
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+
+
+*/
+function spinWords(string){
+    let arr = string.split(' ').map(item => {
+        return item.length >= 5 ? item.split('').reverse().join('') : item
+  
+    })
+
+    console.log(arr)
+
+  }
+
+
+  spinWords("Hey fellow warriors")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
