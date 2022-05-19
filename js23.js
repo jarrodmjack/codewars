10f4ae60f011c421d3c334493ff4c014db235183
jarrodmjack <jarrodmjack@gmail.com>
2022-05-19 00:28:38 -0400

    codewars

--- js23.js
+++ js23.js
@@ -74,11 +74,86 @@ Output:
 */
 
 
-function removeDuplicateWords (s) {
-    let arr = s.split(' ')
+// function removeDuplicateWords (s) {
+//     let arr = s.split(' ')
 
-    let mySet1 = Array.from(new Set(arr))
-    console.log(mySet1.join(' '))
-  }
+//     let mySet1 = Array.from(new Set(arr))
+//     console.log(mySet1.join(' '))
+//   }
 
-  removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta')
+//   removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta')
+
+
+
+
+
+
+
+
+
+
+/*
+Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+The geese are any strings in the following array, which is pre-populated in your solution:
+
+  ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+For example, if this array were passed as an argument:
+
+ ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+Your function would return the following array:
+
+["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+*/
+
+
+
+
+// PREP
+// filter out geese. return geese-less array
+
+
+//Parameters: ---> Function takes in array. always an array. New array cannot contain any geese. array must be returned
+
+
+
+//Returns: Return a new array with no geese inside of it
+
+
+//Example:
+// ['goose', 'chicken', 'goose', 'duck'] ----> ['chicken', 'duck']
+
+
+
+
+
+
+
+function gooseFilter (birds) {
+  let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  
+  
+  //Pseudo: 
+  // create a variable to store new geeseless array in
+  // filter array for geese
+  // check new array to make sure no geese
+
+  let noGeese = birds.filter((item, i) =>  !geese.includes(item))
+  return noGeese
+
+
+
+};
+
+gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"])
+
+
+
+
+
+
+
+
+
+
