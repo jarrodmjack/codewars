73740ebb1c760205af008a514250a75a6504f884
jarrodmjack <jarrodmjack@gmail.com>
2022-05-27 23:45:15 -0400

    codewars [200~Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).

--- js31.js
+++ js31.js
@@ -1,12 +1,27 @@
 
 
 
-/*
-This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+/* 
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
 
-var a == "code";
-var b == "wa.rs";
-var name == a + b;
+Hint for R users:
+
+The length of string is not always the same as the number of characters
+For example: (Input1, Input2) --> output
+
+("1", "22") --> "1221"
+("22", "1") --> "1221"
 */
 
-let name = 'codewa.rs'
+
+
+function solution(a, b){
+    if(a.length > b.length){
+        return `${b}${a}${b}`
+    }else if(b.length > a.length){
+        return `${a}${b}${a}`
+    }
+  }
+
+  console.log(solution('1', '22'))
+  
