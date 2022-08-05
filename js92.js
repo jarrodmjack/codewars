502b3442799bdb1e92988cb165ddaac6cfa4d975
jarrodmjack <jarrodmjack@gmail.com>
2022-08-05 15:59:34 -0400

    refactored

--- js92.js
+++ js92.js
@@ -15,47 +15,32 @@
 
 
 function mostMoney(students) { // taking in students array of objects
-    let arr = [];
-    for (let i = 0; i < students.length; i++) { // for loop through array
-        let sum = [students[i].fives, students[i].tens, students[i].twenties]
-        arr.push(sum)
+
+    let res = [];
+    for(let i = 0; i < students.length; i++){
+        let sum = 0;
+        sum += (students[i].fives * 5)
+        sum += (students[i].tens * 10)
+        sum += (students[i].twenties * 20)
+        res.push(sum)
     }
     
-    console.log(arr)
-    let res = [];
-    for(let i = 0; i < arr.length; i++){
-        let amount = arr[i].reduce((acc, c, idx) => {
-            // console.log(idx)
-            if(idx === 0){
-                acc += (c * 5)
-            }else if(idx === 1){
-                acc += (c * 10)
-            }else if(idx === 2){
-                acc += (c * 20)
-            }
-            return acc
-        }, 0)
-        res.push(amount)
-    }
+    // if(res.every(item, i => item === item[0]))
     console.log(res)
- 
-    if(res.every( (val, i, arr) => val === arr[0]) === true){
-        console.log('All')
+    if(res.every((item, i, arr) => item === arr[0])){
+        return 'All'
     }else{
-        let k = res.indexOf(Math.max(...res))
-        console.log(students[k].name)
+        let k = Math.max(...res)
+        let j = res.indexOf(k)
+       return students[j].name
     }
-
-    // let k = res.indexOf(Math.max(...res))
-    // console.log(students[k])
-  
 
 }
 
-mostMoney([
+console.log(mostMoney([
     { name: 'Andy', fives: 0, tens: 0, twenties: 2 },
     { name: 'Stephen', fives: 0, tens: 4, twenties: 0 },
-    { name: 'Eric', fives: 6, tens: 1, twenties: 0 },
+    { name: 'Eric', fives: 7, tens: 1, twenties: 0 },
     { name: 'David', fives: 4, tens: 0, twenties: 1 },
-    { name: 'Phil', fives: 0, tens: 4, twenties: 1 }
-])
+    { name: 'Phil', fives: 0, tens: 2, twenties: 1 }
+]))
