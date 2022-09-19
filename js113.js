499858852cbcc7b932ec828807f1cc23213875a5
jarrodmjack <jarrodmjack@gmail.com>
2022-09-18 23:23:41 -0400

    leetcode binary search

---
+++ js113.js
@@ -0,0 +1,48 @@
+
+// .sort((a,b) => a - b)
+
+
+// [6,5,4,3,2,1]
+// [5,6,4,3,2,1]
+// [5,4,6,3,2,1]
+// [4,5,6,3,2,1]
+
+
+function sort(arr){
+    for (let i = 0; i < arr.length; ++i) {
+        for (let j = 0; j < arr.length-1-i; ++j) {
+            if (arr[j] > arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+            }
+            console.log(arr)
+        }
+    }
+}
+
+sort([6,5,4,3,2,1])
+
+
+
+var search = function(nums, target) {
+    
+    let low = 0;
+    let high = nums.length
+  
+    while (low < high) {
+      
+      let half = Math.floor((high + low) / 2) 
+
+      if(nums[half] === target){
+        return half
+      }else if(target < nums[half]) {
+        high = half  
+      }else{
+        low = half + 1
+      }
+    }
+
+  return -1
+  
+};
+
+
